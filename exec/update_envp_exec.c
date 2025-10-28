9340fe7c43511ad04e653e902686ef4af7a5186a
Lebroue <lebroue@z2r12p4.42lyon.fr>
2025-10-28 17:24:07 +0100

    normed the update_envp_exec.c functions, test minishell again: no problems appears

--- exec/update_envp_exec.c
+++ exec/update_envp_exec.c
@@ -6,7 +6,7 @@
 /*   By: lebroue <leobroue@student.42lyon.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 16:41:44 by lebroue           #+#    #+#             */
-/*   Updated: 2025/10/28 17:07:54 by lebroue          ###   ########.fr       */
+/*   Updated: 2025/10/28 17:20:58 by lebroue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,42 @@
 #include "parsing.h"
 
 //////////////////////////////////////////////
-// Construire envp  en char ** depuis liste chaînée env
+// Compte le nombre de node de la liste chainee envp du pars
 //////////////////////////////////////////////
-char	**build_envp_from_lst_env(t_env *env)
+int count_node(t_env *tmp, int count)
 {
-	int		count;
-	t_env	*tmp;
-	char	**envp;
-	int		i;
-	char	*tmp2;
-
-	count = 0;
-	tmp = env;
 	while (tmp)
 	{
 		count++;
 		tmp = tmp->next;
 	}
+	return(count);
+}
+//////////////////////////////////////////////
+// Construire envp  en char ** depuis liste chaînée env
+//////////////////////////////////////////////
+char	**build_envp_tab_from_lst_env(t_env *env)
+{
+	int		count;
+	t_env	*tmp;
+	char	**envp;
+	int		i;
+	char	*key_equal;
+
+	count = 0;
+	tmp = env;
+	i = 0;
+	count = count_node(tmp, count); 
 	envp = malloc(sizeof(char *) * (count + 1));
 	if (!envp)
 		return (NULL);
-	i = 0;
 	tmp = env;
 	while (tmp)
 	{
 		envp[i] = ft_strjoin(tmp->key, "=");
-		tmp2 = envp[i];
-		envp[i] = ft_strjoin(tmp2, tmp->value);
-		free(tmp2);
+		key_equal = envp[i];
+		envp[i] = ft_strjoin(key_equal, tmp->value);
+		free(key_equal);
 		tmp = tmp->next;
 		i++;
 	}
@@ -58,5 +66,5 @@ void	update_envp(t_data *data)
 {
 	if (data->envp)
 		free_envp(data->envp);
-	data->envp = build_envp_from_lst_env(data->env);
+	data->envp = build_envp_tab_from_lst_env(data->env);
 }
