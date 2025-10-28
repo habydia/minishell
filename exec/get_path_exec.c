ab82d682f19fe2489bd35d8d5c325467718b4fc8
Lebroue <lebroue@z2r12p4.42lyon.fr>
2025-10-28 18:52:09 +0100

    created the get_path_exec.c file still have to finish it whit the utils funtcions

--- exec/get_path_exec.c
+++ exec/get_path_exec.c
@@ -6,7 +6,7 @@
 /*   By: lebroue <leobroue@student.42lyon.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 17:33:52 by lebroue           #+#    #+#             */
-/*   Updated: 2025/10/28 17:36:29 by lebroue          ###   ########.fr       */
+/*   Updated: 2025/10/28 18:51:26 by lebroue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,6 @@
 #include "env.h"
 #include "parsing.h"
 #include "exec.h"
+
+
+// sortir les fonctions get path de l'exec, et mettre les utils associe 
