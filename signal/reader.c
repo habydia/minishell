d07029271129431657c837bae8167c6d67ee6ce6
lebroue <lebroue@student.42lyon.fr>
2025-11-04 21:55:23 +0100

    trying to manage commits

--- signal/reader.c
+++ signal/reader.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reader.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lebroue <leobroue@student.42lyon.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/04 21:42:00 by lebroue           #+#    #+#             */
+/*   Updated: 2025/11/04 21:42:16 by lebroue          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
 #include "minishell_signals.h"
 
 char	*reader(void)
 {
 	char *line;
 
-	// rl_catch_signals = 0;
 	line = readline("Minishell> ");
 	if (line == NULL)
 		exit(1);
