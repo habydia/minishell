e54fb871b50554e61d598bbc34f9847782d26ef2
lebroue <lebroue@student.42lyon.fr>
2025-11-05 01:30:21 +0100

    exec_builtins, ft_cd normed v2

--- builtins/ft_cd.c
+++ builtins/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: lebroue <leobroue@student.42lyon.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 19:38:29 by lebroue           #+#    #+#             */
-/*   Updated: 2025/11/05 01:24:57 by lebroue          ###   ########.fr       */
+/*   Updated: 2025/11/05 01:29:33 by lebroue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,38 +17,39 @@
 #include <string.h>
 #include <unistd.h>
 
-bool check_to_many_arguments(char ** args)
+bool	check_to_many_arguments(char **args)
 {
 	if (args[1])
 	{
-		if(args[2])
+		if (args[2])
 		{
 			write(2, "cd: too many arguments\n", 24);
 			return (true);
 		}
 	}
-	return(false);
+	return (false);
 }
 
-bool path_checker_limit(char *path, size_t limit)
+bool	path_checker_limit(char *path, size_t limit)
 {
 	if (ft_strlen(path) > limit)
 	{
 		write(2, "cd: File name too long\n", 24);
 		return (true);
 	}
-	return(false);
+	return (false);
 }
-bool check_chdir(int ret)
+
+bool	check_chdir(int ret)
 {
-		if (ret == -1)
+	if (ret == -1)
 	{
 		perror("cd");
 		return (true);
 	}
 	return (false);
-	
 }
+
 int	ft_cd(char **args)
 {
 	char	*path;
@@ -56,10 +57,10 @@ int	ft_cd(char **args)
 	size_t	limit;
 
 	limit = 255;
-	if(check_to_many_arguments(args) == true)
-		return(1);
+	if (check_to_many_arguments(args) == true)
+		return (1);
 	if (!args[1] || (args[1][0] == '~' && (args[1][1] == '\0'
-				|| args[1][1] == '/')))
+		|| args[1][1] == '/')))
 	{
 		path = getenv("HOME");
 		if (!path)
@@ -69,11 +70,11 @@ int	ft_cd(char **args)
 		}
 	}
 	else
-			path = ft_strdup(args[1]);
-	if(path_checker_limit(path, limit) == true)
-			return(1);
+		path = ft_strdup(args[1]);
+	if (path_checker_limit(path, limit) == true)
+		return (1);
 	ret = chdir(path);
-	if(check_chdir(ret) == true)
-		return(1);
-	return(0);
+	if (check_chdir(ret) == true)
+		return (1);
+	return (0);
 }
--- builtins/ft_echo.c
+++ builtins/ft_echo.c
@@ -6,17 +6,18 @@
 /*   By: lebroue <leobroue@student.42lyon.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 19:35:05 by lebroue           #+#    #+#             */
-/*   Updated: 2025/11/05 01:26:55 by lebroue          ###   ########.fr       */
+/*   Updated: 2025/11/05 01:27:49 by lebroue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
-
 #include <unistd.h>
 
 static size_t	count_tab(char **str)
 {
-	size_t i = 0;
+	size_t	i;
+
+	i = 0;
 	while (str && str[i])
 		i++;
 	return (i);
@@ -27,16 +28,15 @@ int	ft_echo(char **str)
 	int		i;
 	int		n_flag;
 	size_t	count;
-	
-	i  = 1;
-	n_flag  = 0;
+
+	i = 1;
+	n_flag = 0;
 	count = count_tab(str);
 	if (count > 1 && str[1][0] == '-' && str[1][1] == 'n' && str[1][2] == '\0')
 	{
 		n_flag = 1;
 		i++;
 	}
-
 	while (str[i])
 	{
 		write(1, str[i], ft_strlen(str[i]));
@@ -46,6 +46,5 @@ int	ft_echo(char **str)
 	}
 	if (!n_flag)
 		write(1, "\n", 1);
-
 	return (0);
 }
