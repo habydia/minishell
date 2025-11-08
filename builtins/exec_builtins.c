233298935545fafddd2e5a2effdc3acd7374f74d
lebroue <lebroue@student.42lyon.fr>
2025-11-08 19:11:58 +0100

    just modified ft_env.c for the case where you write : env e or wath ever you put after

--- builtins/exec_builtins.c
+++ builtins/exec_builtins.c
@@ -6,7 +6,7 @@
 /*   By: lebroue <leobroue@student.42lyon.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 18:32:47 by lebroue           #+#    #+#             */
-/*   Updated: 2025/11/06 23:30:29 by lebroue          ###   ########.fr       */
+/*   Updated: 2025/11/08 19:06:29 by lebroue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,19 +44,19 @@ int	exec_builtins(t_cmd *cmd, t_data *data, char **env, char *input)
 	status = 0;
 	if (!cmd || !cmd->args || !cmd->args[0])
 		return (1);
-	if (ft_strncmp(cmd->args[0], "env", 3) == 0)
-		status = ft_env(env);
-	else if (ft_strncmp(cmd->args[0], "pwd", 3) == 0)
+	if (ft_strcmp(cmd->args[0], "env") == 0)
+		status = ft_env(cmd->args, env);
+	else if (ft_strcmp(cmd->args[0], "pwd") == 0)
 		status = ft_pwd();
-	else if (ft_strncmp(cmd->args[0], "exit", 4) == 0)
+	else if (ft_strcmp(cmd->args[0], "exit") == 0)
 		status = ft_exit_handle(cmd, data, status, input);
-	else if (ft_strncmp(cmd->args[0], "echo", 4) == 0)
+	else if (ft_strcmp(cmd->args[0], "echo") == 0)
 		status = ft_echo(cmd->args);
-	else if (ft_strncmp(cmd->args[0], "cd", 2) == 0)
+	else if (ft_strcmp(cmd->args[0], "cd") == 0)
 		status = ft_cd(cmd->args);
-	else if (ft_strncmp(cmd->args[0], "export", 6) == 0)
+	else if (ft_strcmp(cmd->args[0], "export") == 0)
 		status = ft_export(cmd->args, env, data);
-	else if (ft_strncmp(cmd->args[0], "unset", 5) == 0)
+	else if (ft_strcmp(cmd->args[0], "unset") == 0)
 		status = ft_unset(cmd->args, env, data);
 	else
 	{
