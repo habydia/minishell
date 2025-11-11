10b3cfc5ae6f0ea4a8eb34568b730d4ae759a300
hadia <hadia@student.42lyon.fr>
2025-11-11 04:02:36 +0100

    feat(heredoc): add complete signal handling for heredoc, return 130 on sigint interruption,Implement dedicated SIGINT handler for heredoc, Add warning message when heredoc terminated by EOF (Ctrl-D)

--- parsing/parser/heredoc_handler.c
+++ parsing/parser/heredoc_handler.c
@@ -3,14 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   heredoc_handler.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hadia <hadia@student.42.fr>                +#+  +:+       +#+        */
+/*   By: Hadia <Hadia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 05:00:34 by hadia             #+#    #+#             */
-/*   Updated: 2025/11/01 05:16:45 by hadia            ###   ########.fr       */
+/*   Updated: 2025/11/11 03:37:49 by Hadia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
+#include "minishell_signals.h"
+#include <sys/ioctl.h>
+
+volatile sig_atomic_t g_heredoc_interrupted = 0;
+
+void handle_sigint_heredoc(int sig)
+{
+	(void)sig;
+	g_heredoc_interrupted = 1;
+	write(1, "\n", 1);
+	close(STDIN_FILENO);
+}
+
+void setup_sigint_heredoc(void)
+{
+	struct sigaction	sa_int;
+
+	g_heredoc_interrupted = 0;
+	sa_int.sa_handler = handle_sigint_heredoc;
+	sigemptyset(&sa_int.sa_mask);
+	sa_int.sa_flags = 0;
+	sigaction(SIGINT, &sa_int, NULL);
+}
 
 static char	*generate_temp_filename(void)
 {
@@ -27,22 +50,43 @@ static char	*generate_temp_filename(void)
 	return (filename);
 }
 
-static void	write_in_heredoc_file(int fd, const char *delimiter)
+static int	write_in_heredoc_file(int fd, const char *delimiter)
 {
 	char	*line;
+	int		stdin_backup;
 
+	stdin_backup = dup(STDIN_FILENO);
+	setup_sigint_heredoc();
 	while (1)
 	{
 		line = readline("> ");
-		if (!line || strcmp(line, delimiter) == 0)
+		if (g_heredoc_interrupted)
 		{
 			free(line);
-			break ;
+			dup2(stdin_backup, STDIN_FILENO);
+			close(stdin_backup);
+			handle_signals();
+			return (-1);
+		}
+		if (!line)
+		{
+			ft_putstr_fd("minishell: warning: here-document delimited by end-of-file (wanted `", 2);
+			ft_putstr_fd((char *)delimiter, 2);
+			ft_putstr_fd("')\n", 2);
+			break;
+		}
+		if (strcmp(line, delimiter) == 0)
+		{
+			free(line);
+			break;
 		}
 		write(fd, line, strlen(line));
 		write(fd, "\n", 1);
 		free(line);
 	}
+	close(stdin_backup);
+	handle_signals();
+	return (0);
 }
 
 void	handle_heredoc(t_redir *redir)
@@ -67,7 +111,16 @@ void	handle_heredoc(t_redir *redir)
 		free(delimiter);
 		return ;
 	}
-	write_in_heredoc_file(fd, delimiter);
+	if(write_in_heredoc_file(fd, delimiter) < 0)
+	{
+		 close(fd);
+   		 unlink(temp_filename);  // Supprimer le fichier !
+   		 free(temp_filename);
+   		 free(delimiter);
+   		 redir->file = NULL;
+		 g_exit_status = 130;
+   		 return ;
+	}
 	close(fd);
 	free(redir->file);
 	free(delimiter);
